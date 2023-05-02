346162cf1b4ec32f0da0aef9b8ba43e745ca874c
Jules Tréhorel <jules.trehorel@gmail.com>
2023-05-02 21:56:09 +0200

    (fix) fix OGS correspondance game conditions

--- app/src/main/kotlin/com/fulgurogo/features/user/ogs/OgsGame.kt
+++ app/src/main/kotlin/com/fulgurogo/features/user/ogs/OgsGame.kt
@@ -18,21 +18,17 @@ data class OgsGame(
     @SerializedName("annulled") val cancelled: Boolean = false,
     val width: Int = 0,
     val height: Int = 0,
-    @SerializedName("time_per_move") val timePerMove: Int = 0,
-    @SerializedName("time_control") val timeControl: String = "",
-    @SerializedName("time_control_parameters") val timeControlParams: String = "",
     val handicap: Int = 0,
     val rules: String = "",
     val komi: String = "",
     val rengo: Boolean = false,
     @SerializedName("disable_analysis") val analysisDisabled: Boolean = false,
     val outcome: String = "",
+    @SerializedName("gamedata") val gameData: OgsGameData? = null
 ) : UserAccountGame() {
     companion object {
         private const val DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSX"
         private const val DATE_FORMAT_OLD = "yyyy-MM-dd'T'HH:mm:ssX"
-        private const val BLITZ_TIME_PER_MOVE = 20
-        private const val CORRESPONDENCE_TIME_PER_MOVE = 89280
     }
 
     override fun date(): Date = try {
@@ -64,37 +60,12 @@ data class OgsGame(
     override fun isFinished(): Boolean = outcome.isNotBlank()
     override fun handicap(): Int = handicap
     override fun komi(): Double = komi.toDouble()
-    override fun isLongGame(): Boolean = isLiveGame() && when (timeControl) {
-        "byoyomi", "canadian" -> extractTime("main_time") >= 1200
-        "fischer" -> extractTime("initial_time") >= 600 && extractTime("time_increment") >= 20
-        "absolute" -> extractTime("total_time") >= 2400
-        else -> false
-    }
+    override fun isLongGame(): Boolean = gameData?.timeControl?.isLongGame() ?: false
 
     private fun isDraw() = outcome == "0 points"
     fun isNotCancelled(): Boolean = !cancelled
-    fun isLiveGame(): Boolean =
-        (timeControlParams.contains("\"speed\": \"live\"") || timePerMove in (BLITZ_TIME_PER_MOVE + 1) until CORRESPONDENCE_TIME_PER_MOVE)
-
-    fun isCorrespondenceGame(): Boolean =
-        timeControlParams.contains("\"speed\": \"correspondence\"") || timePerMove >= CORRESPONDENCE_TIME_PER_MOVE
-
+    fun isCorrespondenceGame(): Boolean = gameData?.timeControl?.isCorrespondenceGame() ?: true
     fun isNineteen(): Boolean = width == 19 && height == 19
     fun isNotBotGame(): Boolean = !historicalRatings.black.isBot() && !historicalRatings.white.isBot()
     fun isRengo(): Boolean = rengo
-
-    private fun extractTime(key: String): Int {
-        var time: String = timeControlParams.substring(timeControlParams.indexOf("\"$key\": "))
-        time = time.substring(("\"$key\": ").length)
-
-        var end = time.indexOf(",")
-        if (end == -1) end = time.indexOf("}")
-        if (end == -1) end = time.length
-        time = time.substring(0, end)
-        return try {
-            time.toInt()
-        } catch (e: NumberFormatException) {
-            0
-        }
-    }
 }
