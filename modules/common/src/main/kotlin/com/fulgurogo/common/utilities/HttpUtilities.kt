7be4c293f1c39efcf5bb16feaaaba06ef4482451
Jules Tréhorel <jules.trehorel@iadvize.com>
2025-09-21 13:47:53 +0200

    (fix) udpate scraper

--- modules/common/src/main/kotlin/com/fulgurogo/common/utilities/HttpUtilities.kt
+++ modules/common/src/main/kotlin/com/fulgurogo/common/utilities/HttpUtilities.kt
@@ -3,25 +3,12 @@ package com.fulgurogo.common.utilities
 import com.fulgurogo.common.config.Config
 import okhttp3.JavaNetCookieJar
 import okhttp3.OkHttpClient
+import org.jsoup.Connection
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import java.net.CookieManager
 import java.net.CookiePolicy
 import java.util.concurrent.TimeUnit
-
-// Rnadom Free proxies from https://free-proxy-list.net/fr/
-private val proxies = listOf(
-    "170.130.202.134" to 3128,
-    "152.53.168.53" to 44887,
-    "65.108.203.37" to 18080,
-    "18.135.100.214" to 3128,
-    "162.212.153.46" to 8888,
-    "209.121.164.50" to 31147,
-    "92.67.186.210" to 80,
-    "45.146.163.31" to 80,
-    "20.27.15.111" to 8561,
-    "5.188.183.253" to 8080
-)
 
 fun okHttpClient(): OkHttpClient = OkHttpClient.Builder()
     .connectTimeout(Config.get("global.read.timeout.ms").toLong(), TimeUnit.MILLISECONDS)
@@ -29,11 +16,30 @@ fun okHttpClient(): OkHttpClient = OkHttpClient.Builder()
     .cookieJar(JavaNetCookieJar(CookieManager().apply { setCookiePolicy(CookiePolicy.ACCEPT_ALL) }))
     .build()
 
+private val scraperCookies: MutableMap<String, String> = mutableMapOf()
+
 fun scrap(url: String): Document {
-    val proxy = proxies.random()
-    return Jsoup.connect(url)
+    val response: Connection.Response = Jsoup.connect(url)
+        .header(
+            "Accept",
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8"
+        )
+        .header("Accept-Language", "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7")
+        .header("Accept-Encoding", "gzip, deflate, br")
+        .header("Connection", "keep-alive")
+        .header("Upgrade-Insecure-Requests", "1")
+        .header("Sec-Fetch-Dest", "document")
+        .header("Sec-Fetch-Mode", "navigate")
+        .header("Sec-Fetch-Site", "none")
+        .header("Cache-Control", "max-age=0")
+        .followRedirects(true)
         .userAgent(Config.get("user.agent"))
+        .referrer("https://www.google.com")
         .timeout(Config.get("global.read.timeout.ms").toInt())
-        .proxy(proxy.first, proxy.second)
-        .get()
+        .method(Connection.Method.GET)
+        .execute()
+
+    scraperCookies.putAll(response.cookies())
+
+    return response.parse()
 }
